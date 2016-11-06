570920272eefe3f86231684bfd145d6ac453a882
lima1756 <luisivanmorett@gmail.com>
2016-11-06 09:16:10 -0600

    Proyecto visual studio
    Incluye algo de codigo que estuve probando

---
+++ compilador/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compilador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine(Environment.GetCommandLineArgs()[1]);
+            Console.ReadKey();
+        }
+    }
+}
